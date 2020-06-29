e33fda80a9e1619a0249a717b5f4b0364a7bfbeb
shubhambattoo <shubham.battoo@hotmail.com>
2020-06-29 21:22:07 +0530

    added service worker and offline uses

---
+++ public/serviceworker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'version-1';
+
+const urls = ['index.html', 'offline.html'];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('opened cached');
+
+      return cache.addAll(urls);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match('offline.html'));
+    })
+  );
+});
+// Activate SW
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
